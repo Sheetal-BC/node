46d25e7c64c23c2b9d26f0694d2efd87047150ac
Sheetal BC <93135955+Sheetal-BC@users.noreply.github.com>
2023-01-13 15:04:56 +0530

    created-server

---
+++ app.js
@@ -0,0 +1,9 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Sheetal");
+});
+
+server.listen(4000);
