07a3547898982d96a3cbe2abc5fc4be372465872
Sheetal BC <93135955+Sheetal-BC@users.noreply.github.com>
2023-01-13 16:21:25 +0530

    custom-response-added

--- app.js
+++ app.js
@@ -1,9 +1,45 @@
-const http = require("http");
+// const http = require("http");
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Sheetal");
-});
+// const server = http.createServer((req, res) => {
+    
+//   res.statusCode = 200;
+//   res.setHeader("Content-Type", "text/plain");
+//   res.end("Sheetal");
+// });
 
-server.listen(4000);
+// server.listen(4000);
+
+const http = require('http');
+
+const server = http.createServer((req,res)=>{
+    const url = req.url;
+    if(url === "/home"){
+      res.write("<html>");
+      res.write("<head><title>My first page</title></head>");
+      res.write("<body><h1>Welcome to Home</h1></body>");
+      res.write("</head>");
+      return res.end();  
+    }else if(url === "/about"){
+       res.write("<html>");
+       res.write("<head><title>My first page</title></head>");
+       res.write("<body><h1>Welcome to About page</h1></body>");
+       res.write("</head>");
+       return res.end(); 
+    }else if(url === "/node"){
+         res.write("<html>");
+       res.write("<head><title>My first page</title></head>");
+       res.write("<body><h1>Welcome to my Node Js project</body>");
+       res.write("</head>");
+       return res.end(); 
+    }else{
+       res.setHeader("Content-Type","text/html");
+   res.write("<html>")
+   res.write("<head><title>My first page</title></head>")
+   res.write("<body><h1>Welcome to my Node Js project</h1></body>")
+   res.write("</head>")
+   res.end() 
+    }
+   
+})
+
+server.listen(4000)
--- first-app.js
+++ first-app.js
@@ -1,4 +1,68 @@
-const fs = require("fs");
-fs.writeFileSync("hello.txt", "Hello world")
+// const fs = require("fs");
+// fs.writeFileSync("hello.txt", "Hello world")
 
-console.log("hello world")
+// console.log("hello world")
+
+// const prod = (num1, num2) => {
+//   return num1 * num2;
+// };
+
+// console.log(prod(2,5));
+
+// const Student = {
+//   name: "Sheethal",
+//   age: 27,
+//   skills: ["HTML", "CSS", "JavaScript"],
+//   printDetails() {
+//     console.log(
+//       `My name is ${this.name} and my age is ${this.age} and my skills are ${this.skills}`
+//     );
+//   },
+// };
+
+// Student.printDetails();
+
+//  const array = ['apple', 'oranges' , ' ', 'mango', ' ' , 'lemon'];
+//  const newArray = array.map((item) =>{
+//    if(item === " "){
+//     return "empty string"
+//    }else{
+//     return item
+//    }
+//  })
+
+//  console.log(newArray)
+
+// const obj1 = { key1: 1, key2: 2 };
+
+// const obj2 = { ...obj1, key1: 1000 };
+
+// console.log(obj1);
+
+// console.log(obj2);
+
+//  const arr1 = ["value1", "value2"];
+
+//  const [val1, val2] = arr1;
+
+//  console.log(val1);
+
+//  console.log(val2);
+
+// let res = new Promise((resolve) => {
+//   setTimeout(() => resolve("c"), 3000);
+// });
+// let res2 = new Promise((resolve) => {
+//   setTimeout(() => resolve("d"), 3000);
+// });
+
+// const newFun = async () => {
+//   console.log("a");
+//   console.log("b");
+//   let result1 = await res;
+//   console.log(result1);
+//   let result2 = await res2;
+//   console.log(result2);
+//   console.log("e");
+// };
+
